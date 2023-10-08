3865610c86860fcf2088dfe41cf7e1b51eb6b010
shivPosted <shivpratap621@gmail.com>
2023-10-08 22:38:05 +0530

    playing with JS selectors pt3 😄

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 const playlistContainer = document.querySelector(
   ".display-container-playlists"
 );
+const containerPlaylistSong = document.querySelector(".playlist-songs");
 
 playlistContainer.addEventListener("click", function (e) {
   if (!e.target.closest(".playlist-card")) return;
   console.log(e.target);
   const playlistName = e.target.dataset.playlistName;
+  playlistContainer.classList.add("hidden");
+  setTimeout(() => {
+    playlistContainer.style.display = "none";
+  }, 500);
+  containerPlaylistSong.classList.remove("hidden");
 });
